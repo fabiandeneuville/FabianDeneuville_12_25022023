4cdc9b4b8e284f2637c7bfc934353136780da937
Fabian Deneuville <deneuville.fabian@live.fr>
2023-02-26 20:33:42 +0100

    Resizing radar chart

--- src/components/ChartPerformance.jsx
+++ src/components/ChartPerformance.jsx
@@ -4,7 +4,7 @@ function ChartPerformance(props){
     return (
         <div className="dashboard__layout__performance">
             <ResponsiveContainer width="100%"height="100%">
-                <RadarChart cx="50%" cy="50%" outerRadius="60%" data={props.performance}>
+                <RadarChart cx="50%" cy="50%" outerRadius="45%" data={props.performance}>
                     <PolarGrid stroke='#fff'/>
                     <PolarAngleAxis stroke='#fff' dataKey="kind"/>
                     <Radar dataKey="value" stroke="rgb(234,51,36)" fill="rgb(234,51,36)" fillOpacity={0.6} />
