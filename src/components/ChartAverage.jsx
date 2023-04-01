4d024d814885ffdf3e635428416bf1a2e9cef9a0
Fabian Deneuville <deneuville.fabian@live.fr>
2023-04-01 23:21:45 +0200

    Dynamically changing chart background

--- src/components/ChartAverage.jsx
+++ src/components/ChartAverage.jsx
@@ -4,21 +4,30 @@ import CustomTooltip from "./CustomTooltip";
 function Title(){
     return (
         <g textAnchor="left">
-        <text fontSize='1em' fill='white' x='10%' y='10%'>
+        <text fontSize='1em' fill='white' x='10%' y='10%' opacity={'0.8'}>
         Durée moyenne des
         </text>
-        <text fontSize='1em' fill='white' x='10%' y='15%'>
+        <text fontSize='1em' fill='white' x='10%' y='15%' opacity={'0.8'}>
         sessions
         </text>
     </g>
-    )
+    );
 }
 
 function ChartAverage(props){
 
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={props.sessions} strokeWidth={1}>
+            <LineChart data={props.sessions} strokeWidth={1}
+                onMouseMove={(e) => {
+                    if (e.isTooltipActive === true) {
+                        let chartContainer = document.querySelector('.dashboard__layout__average');
+                        let windowWidth = chartContainer.clientWidth;
+                        let mouseXpercentage = Math.round((e.activeCoordinate.x / windowWidth) * 100);
+                        chartContainer.style.background = `linear-gradient(90deg, rgb(234,51,36) ${mouseXpercentage}%, rgba(175,0,0,1.5) ${mouseXpercentage}%, rgba(175,0,0,1.5) 100%)`;
+                    };
+                }}>
+                
                 <XAxis
                 type="category"
                 dataKey="day"
